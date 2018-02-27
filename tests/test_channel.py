2068b955ca5f7552c8a923102d40a742d3e9db4c
Robert Kindwall <kindwall@kth.se>
2018-02-27 19:45:29 +0100

    Add tests for channel

---
+++ tests/test_channel.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2003-2007  John Rochester <john@jrochester.org>
+#
+# This file is part of paramiko.
+#
+# Paramiko is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# Paramiko is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Paramiko; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+
+"""
+Unit tests for Channel
+"""
+
+import unittest
+
+from paramiko.channel import Channel
+
+class ChannelTest(unittest.TestCase):
+    def test_get_id(self):
+        channel = Channel(1)
+        self.assertEquals(1, channel.get_id())
+
+    def test_get_name(self):
+        channel = Channel(1)
+        self.assertEquals("1", channel.get_name())
+        channel.set_name("abc")
+        self.assertEquals("abc", channel.get_name())
